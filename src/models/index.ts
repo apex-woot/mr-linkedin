869b6aab19ee5207aad975c3d4f90c1f2ccc4df3
Vitalii Kohut <vkohut@intelliarts.com>
2026-02-06 14:39:00 +0200

    chore: remove non-person modules and legacy selectors

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,1 @@
-export * from './company'
-export * from './job'
 export * from './person'
-export * from './post'
--- src/scrapers/index.ts
+++ src/scrapers/index.ts
@@ -1,13 +1,5 @@
 // Functional scraper APIs
 
-export type { CompanyScraperOptions } from './company'
-export { scrapeCompany } from './company'
-export type { CompanyPostsScraperOptions } from './company_posts'
-export { scrapeCompanyPosts } from './company_posts'
-export type { JobScraperOptions } from './job'
-export { scrapeJob } from './job'
-export type { JobSearchOptions, JobSearchParams } from './job_search'
-export { JobSearchParamsSchema, searchJobs } from './job_search'
 export type { PersonScraperOptions } from './person'
 export { scrapePerson } from './person'
 
